f1716a4057fa8deed132cf99c82abc41e072eec1
deeptadevkota <deeptadevkota21@gmail.com>
2021-06-22 14:45:12 +0545

    front_end_completed

--- video_conferencing/urls.py
+++ video_conferencing/urls.py
@@ -3,4 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('<str:room_id>/<str:user_name>/', home_page, name="home_page"),
+    path('login/', login_page, name="login_page"),
+    path('register/', register_page, name="register_page"),
+    path('dashboard/', dashboard_page, name="dashboard_page"),
 ]
--- video_conferencing/views.py
+++ video_conferencing/views.py
@@ -3,5 +3,18 @@ from .models import *
 
 # Create your views here.
 
-def home_page(request,room_id,user_name):
-    return render(request, 'video_conferencing/home.html',{'room_id':room_id,'user_name':user_name})
+
+def home_page(request, room_id, user_name):
+    return render(request, 'video_conferencing/home.html', {'room_id': room_id, 'user_name': user_name})
+
+
+def login_page(request):
+    return render(request, 'video_conferencing/login.html', {})
+
+
+def register_page(request):
+    return render(request, 'video_conferencing/register.html', {})
+
+
+def dashboard_page(request):
+    return render(request, 'video_conferencing/dashboard.html', {})
