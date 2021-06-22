d8984fd4c56ed5eeb5c7f06e5b263ba022556098
deeptadevkota <deeptadevkota21@gmail.com>
2021-06-22 14:15:28 +0545

    db-setup

--- WebRtc/settings.py
+++ WebRtc/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'WebRtc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'teams_clone',
+        'USER': 'teams_clone',
+        'PASSWORD': 'teams_clone',
+        'HOST': 'localhost',
+        'PORT': '3306'
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
