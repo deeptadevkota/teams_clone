b7d5e5a2d4b6b05aa774497c5f77173daf9e74de
deeptadevkota <deeptadevkota21@gmail.com>
2021-06-25 23:53:14 +0545

    bug fixed

--- video_conferencing/admin.py
+++ video_conferencing/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Team)
+
 admin.site.register(User_Team)
--- video_conferencing/models.py
+++ video_conferencing/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 
 
 
-class Team(models.Model):
-    team_id = models.AutoField(primary_key=True)
-    team_name = models.CharField(max_length=200, null=False)
-    data_formed = models.DateField(auto_now=True)
+
 
 class User_Team(models.Model):
     user_name = models.CharField(User, max_length=200)
     team_id = models.IntegerField()
+    team_name=models.CharField(max_length=200, default="demo")
     is_admin=models.BooleanField(default=False)
