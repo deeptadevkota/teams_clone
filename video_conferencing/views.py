56530da6c34b30a3e0cdcd0903c75bf8e3630035
deeptadevkota <deeptadevkota21@gmail.com>
2021-07-04 14:07:01 +0545

    bug fixed

--- video_conferencing/views.py
+++ video_conferencing/views.py
@@ -143,8 +143,8 @@ def add_members_page(request, team_id, name):
 
 
 @login_required
-def home_page(request, room_id, user_name):
-    return render(request, 'video_conferencing/home.html', {'room_id': room_id, 'user_name': user_name})
+def home_page(request, team_id, user_name):
+    return render(request, 'video_conferencing/home.html', {'team_id': team_id, 'user_name': user_name})
 
 
 @login_required
